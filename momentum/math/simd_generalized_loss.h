6497725f3baa33a5bc7482441696009ff206d61c
Jeongseok Lee <jeongseok@meta.com>
2025-05-29 20:19:09 -0700

    Add tests for simd_generalized_loss (#320)
    Summary: Pull Request resolved: https://github.com/facebookresearch/momentum/pull/320
    
    Differential Revision: D75653553

--- momentum/math/simd_generalized_loss.h
+++ momentum/math/simd_generalized_loss.h
@@ -18,8 +18,22 @@ class SimdGeneralizedLossT : public GeneralizedLossT<T> {
  public:
   using Base = GeneralizedLossT<T>;
 
+  /// Constructs a SIMD generalized loss function
+  ///
+  /// @param a Alpha parameter controlling the shape of the loss function
+  /// @param c Scale parameter controlling the width of the quadratic region
   explicit SimdGeneralizedLossT(const T& a = Base::kL2, const T& c = T(1));
+
+  /// Computes the loss value for a given squared error
+  ///
+  /// @param sqrError Squared error term (SIMD packet)
+  /// @return Loss value as a SIMD packet
   [[nodiscard]] Packet<T> value(const Packet<T>& sqrError) const;
+
+  /// Computes the derivative of the loss function
+  ///
+  /// @param sqrError Squared error term (SIMD packet)
+  /// @return Derivative value as a SIMD packet
   [[nodiscard]] Packet<T> deriv(const Packet<T>& sqrError) const;
 };
 
