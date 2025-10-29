d0e6b01d9a4fd20123161d1ec79f3268187a67ae
Chris Twigg <cdtwigg@meta.com>
2025-10-29 09:10:32 -0700

    Move locators into their own bindings file. (#735)
    Summary:
    
    To simplify geometry_stubgen.
    
    Reviewed By: jeongseok-meta
    
    Differential Revision: D85458675

--- pymomentum/geometry/geometry_pybind.cpp
+++ pymomentum/geometry/geometry_pybind.cpp
@@ -8,6 +8,7 @@
 #include "pymomentum/geometry/character_pybind.h"
 #include "pymomentum/geometry/gltf_builder_pybind.h"
 #include "pymomentum/geometry/limit_pybind.h"
+#include "pymomentum/geometry/locators_pybind.h"
 #include "pymomentum/geometry/mesh_pybind.h"
 #include "pymomentum/geometry/momentum_geometry.h"
 #include "pymomentum/geometry/momentum_io.h"
@@ -281,141 +282,6 @@ The resulting shape is equal to the base shape plus a linear combination of the 
           py::arg("coeffs"))
       .def("__repr__", [](const mm::BlendShape& bs) {
         return fmt::format("BlendShape(shapes={}, vertices={})", bs.shapeSize(), bs.modelSize());
-      });
-
-  // =====================================================
-  // momentum::Locator
-  // - name
-  // - parent
-  // - offset
-  // =====================================================
-  locatorClass
-      .def(
-          py::init<
-              const std::string&,
-              const size_t,
-              const Eigen::Vector3f&,
-              const Eigen::Vector3i&,
-              float,
-              const Eigen::Vector3f&,
-              const Eigen::Vector3f&>(),
-          py::arg("name") = "uninitialized",
-          py::arg("parent") = mm::kInvalidIndex,
-          py::arg("offset") = Eigen::Vector3f::Zero(),
-          py::arg("locked") = Eigen::Vector3i::Zero(),
-          py::arg("weight") = 1.0f,
-          py::arg("limit_origin") = Eigen::Vector3f::Zero(),
-          py::arg("limit_weight") = Eigen::Vector3f::Zero())
-      .def_readonly("name", &mm::Locator::name, "The locator's name.")
-      .def_readonly("parent", &mm::Locator::parent, "The locator's parent joint index.")
-      .def_readonly(
-          "offset", &mm::Locator::offset, "The locator's offset to parent joint location.")
-      .def_readonly(
-          "locked",
-          &mm::Locator::locked,
-          "Flag per axes to indicate whether that axis can be moved during optimization or not.")
-      .def_readonly(
-          "weight", &mm::Locator::weight, "Weight for this locator during IK optimization.")
-      .def_readonly(
-          "limit_origin",
-          &mm::Locator::limitOrigin,
-          "Defines the limit reference position. equal to offset on loading.")
-      .def_readonly(
-          "limit_weight",
-          &mm::Locator::limitOrigin,
-          "Defines how close an unlocked locator should stay to it's original position")
-      .def("__repr__", [](const mm::Locator& l) {
-        return fmt::format(
-            "Locator(name={}, parent={}, offset=[{}, {}, {}])",
-            l.name,
-            l.parent,
-            l.offset.x(),
-            l.offset.y(),
-            l.offset.z());
-      });
-
-  // ==============================================>>>>>>> REPLACE
-  // momentum::SkinnedLocator
-  // - name
-  // - parents
-  // - skinWeights
-  // - position
-  // - weight
-  // =====================================================
-  skinnedLocatorClass
-      .def(
-          py::init([](const std::string& name,
-                      const Eigen::VectorXi& parents,
-                      const Eigen::VectorXf& skinWeights,
-                      const std::optional<Eigen::Vector3f>& position,
-                      float weight) {
-            if (parents.size() != skinWeights.size()) {
-              throw std::runtime_error("parents and skin_weights must have the same size");
-            }
-
-            if (parents.size() > mm::kMaxSkinJoints) {
-              throw std::runtime_error(
-                  fmt::format(
-                      "parents and skin_weights must have at most {} elements",
-                      mm::kMaxSkinJoints));
-            }
-
-            Eigen::Matrix<uint32_t, mm::kMaxSkinJoints, 1> parentsTmp =
-                Eigen::Matrix<uint32_t, mm::kMaxSkinJoints, 1>::Zero();
-            Eigen::Matrix<float, mm::kMaxSkinJoints, 1> skinWeightsTmp =
-                Eigen::Matrix<float, mm::kMaxSkinJoints, 1>::Zero();
-
-            for (size_t i = 0; i < parents.size(); ++i) {
-              if (parents(i) < 0) {
-                throw std::runtime_error(
-                    "parents must be non-negative, but got " + std::to_string(parents(i)));
-              }
-
-              if (skinWeights(i) < 0) {
-                throw std::runtime_error(
-                    "skin_weights must be non-negative, but got " + std::to_string(skinWeights(i)));
-              }
-              parentsTmp(i) = parents(i);
-              skinWeightsTmp(i) = skinWeights(i);
-            }
-
-            return mm::SkinnedLocator(
-                name,
-                parentsTmp,
-                skinWeightsTmp,
-                position.value_or(Eigen::Vector3f::Zero()),
-                weight);
-          }),
-          py::arg("name"),
-          py::arg("parents"),
-          py::arg("skin_weights"),
-          py::arg("position") = std::nullopt,
-          py::arg("weight") = 1.0f)
-      .def_readonly("name", &mm::SkinnedLocator::name, "The skinned locator's name.")
-      .def_property_readonly(
-          "parents",
-          [](const mm::SkinnedLocator& locator) { return locator.parents; },
-          "Indices of the parent joints in the skeleton.")
-      .def_property_readonly(
-          "skin_weights",
-          [](const mm::SkinnedLocator& locator) { return locator.skinWeights; },
-          "Skinning weights for the parent joints.")
-      .def_readonly(
-          "position",
-          &mm::SkinnedLocator::position,
-          "Position relative to rest pose of the character.")
-      .def_readonly(
-          "weight",
-          &mm::SkinnedLocator::weight,
-          "Influence weight of this locator when used in constraints.")
-      .def("__repr__", [](const mm::SkinnedLocator& l) {
-        return fmt::format(
-            "SkinnedLocator(name={}, position=[{}, {}, {}], weight={})",
-            l.name,
-            l.position.x(),
-            l.position.y(),
-            l.position.z(),
-            l.weight);
       });
 
   // =====================================================
@@ -1168,6 +1034,8 @@ The character has only one parameter limit: min-max type [-0.1, 0.1] for root.
   registerSkeletonBindings(skeletonClass);
 
   registerSkinWeightsBindings(skinWeightsClass);
+
+  registerLocatorBindings(locatorClass, skinnedLocatorClass);
 
   registerParameterTransformBindings(parameterTransformClass);
   registerInverseParameterTransformBindings(inverseParameterTransformClass);
