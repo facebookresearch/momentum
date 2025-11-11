80749d30cad8e739604a63b424b734484a0e12c7
Carsten Stoll <cstoll@meta.com>
2025-11-11 15:43:48 -0800

    Joint-to-joint error function (#805)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/momentum/pull/805
    
    Added an error function that calculates a loss for the difference between two points attached to (different) joints (e.g. locators etc).
    
    Reviewed By: jeongseok-meta, cdtwigg
    
    Differential Revision: D86689165
    
    fbshipit-source-id: c356be6ce4abfde6409e006d759c084a34a8f819

--- momentum/test/character_solver/error_functions_test.cpp
+++ momentum/test/character_solver/error_functions_test.cpp
@@ -24,6 +24,7 @@
 #include "momentum/character_solver/distance_error_function.h"
 #include "momentum/character_solver/fixed_axis_error_function.h"
 #include "momentum/character_solver/fwd.h"
+#include "momentum/character_solver/joint_to_joint_distance_error_function.h"
 #include "momentum/character_solver/limit_error_function.h"
 #include "momentum/character_solver/model_parameters_error_function.h"
 #include "momentum/character_solver/normal_error_function.h"
@@ -1954,5 +1955,86 @@ TYPED_TEST(Momentum_ErrorFunctionsTest, DistanceConstraint_GradientsAndJacobians
       TEST_GRADIENT_AND_JACOBIAN(
           T, &errorFunction, parameters, character, Eps<T>(1e-1f, 2e-5), Eps<T>(1e-6f, 1e-7));
     }
+  }
+}
+
+TYPED_TEST(Momentum_ErrorFunctionsTest, JointToJointDistanceError_GradientsAndJacobians) {
+  using T = typename TestFixture::Type;
+
+  const Character character = createTestCharacter();
+  const Skeleton& skeleton = character.skeleton;
+  const ParameterTransformT<T> transform = character.parameterTransform.cast<T>();
+
+  JointToJointDistanceErrorFunctionT<T> errorFunction(skeleton, character.parameterTransform);
+  const T TEST_WEIGHT_VALUE = 2.5;
+
+  {
+    SCOPED_TRACE("JointToJoint Distance Constraint Test");
+
+    ASSERT_GE(skeleton.joints.size(), 3);
+
+    errorFunction.addConstraint(
+        1,
+        uniform<Vector3<T>>(-1, 1),
+        2,
+        uniform<Vector3<T>>(-1, 1),
+        uniform<T>(0.5, 2.0),
+        TEST_WEIGHT_VALUE);
+
+    errorFunction.addConstraint(
+        0, Vector3<T>::Zero(), 2, Vector3<T>::UnitX(), uniform<T>(1.0, 3.0), TEST_WEIGHT_VALUE);
+
+    if constexpr (std::is_same_v<T, float>) {
+      TEST_GRADIENT_AND_JACOBIAN(
+          T,
+          &errorFunction,
+          ModelParametersT<T>::Zero(transform.numAllModelParameters()),
+          character,
+          5e-2f);
+    } else if constexpr (std::is_same_v<T, double>) {
+      TEST_GRADIENT_AND_JACOBIAN(
+          T,
+          &errorFunction,
+          ModelParametersT<T>::Zero(transform.numAllModelParameters()),
+          character,
+          1e-8,
+          1e-6,
+          true,
+          false);
+    }
+
+    for (size_t i = 0; i < 10; i++) {
+      ModelParametersT<T> parameters =
+          uniform<VectorX<T>>(transform.numAllModelParameters(), -1.0f, 1.0f);
+      TEST_GRADIENT_AND_JACOBIAN(
+          T, &errorFunction, parameters, character, Eps<T>(1e-1f, 2e-5), Eps<T>(1e-6f, 1e-7));
+    }
+  }
+
+  {
+    SCOPED_TRACE("JointToJoint Distance Zero Error Test");
+
+    const ModelParametersT<T> modelParams =
+        ModelParametersT<T>::Zero(transform.numAllModelParameters());
+    const SkeletonStateT<T> skelState(transform.apply(modelParams), skeleton);
+
+    JointToJointDistanceErrorFunctionT<T> errorFunctionZero(skeleton, character.parameterTransform);
+
+    const auto& jointState1 = skelState.jointState[1];
+    const auto& jointState2 = skelState.jointState[2];
+
+    const auto offset1 = uniform<Vector3<T>>(-1, 1);
+    const auto offset2 = uniform<Vector3<T>>(-1, 1);
+
+    const Vector3<T> worldPos1 = jointState1.transform * offset1;
+    const Vector3<T> worldPos2 = jointState2.transform * offset2;
+
+    const T actualDistance = (worldPos1 - worldPos2).norm();
+
+    errorFunctionZero.addConstraint(1, offset1, 2, offset2, actualDistance, T(1.0));
+
+    const double error = errorFunctionZero.getError(
+        modelParams, skelState, MeshStateT<T>(modelParams, skelState, character));
+    EXPECT_NEAR(error, 0.0, Eps<T>(1e-10f, 1e-15));
   }
 }
