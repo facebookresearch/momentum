8940d72e33af96c40e9c6e94c0e330537ec3d165
Yuting Ye <yuting.ye@meta.com>
2025-11-15 14:49:08 -0800

    Add parameterized constructor for FileSaveOptions Python binding (#825)
    Summary:
    
    This change adds a parameterized constructor to the FileSaveOptions Python binding, enabling users to initialize all member variables directly when creating the object instead of having to set them one by one after construction.
    
    This improves the API ergonomics by allowing cleaner initialization patterns:
    ```python
    options = FileSaveOptions(mesh=False, locators=True, blend_shapes=False)
    ```
    
    Reviewed By: cstollmeta, jeongseok-meta
    
    Differential Revision: D86904749

--- pymomentum/geometry/gltf_builder_pybind.cpp
+++ pymomentum/geometry/gltf_builder_pybind.cpp
@@ -82,11 +82,12 @@ Setting this value will affect subsequently added motions and animations.
              const mm::Character& character,
              const std::optional<Eigen::Vector3f>& positionOffset,
              const std::optional<Eigen::Vector4f>& rotationOffset,
-             const mm::GltfOptions& options) {
+             const std::optional<mm::GltfOptions>& options) {
             // Use defaults if not provided
             Eigen::Vector3f actualPositionOffset = positionOffset.value_or(Eigen::Vector3f::Zero());
             Eigen::Vector4f actualRotationOffset =
                 rotationOffset.value_or(Eigen::Vector4f(0.0f, 0.0f, 0.0f, 1.0f));
+            mm::GltfOptions actualOptions = options.value_or(mm::GltfOptions{});
 
             // Convert Vector4f (x,y,z,w) to Quaternionf (w,x,y,z)
             mm::Quaternionf quaternionOffset(
@@ -95,7 +96,7 @@ Setting this value will affect subsequently added motions and animations.
                 actualRotationOffset[1], // y
                 actualRotationOffset[2]); // z
 
-            builder.addCharacter(character, actualPositionOffset, quaternionOffset, options);
+            builder.addCharacter(character, actualPositionOffset, quaternionOffset, actualOptions);
           },
           R"(Add a character to the GLTF scene.
 
@@ -113,7 +114,7 @@ can be provided as an initial transform for the character.
           py::arg("character"),
           py::arg("position_offset") = std::nullopt,
           py::arg("rotation_offset") = std::nullopt,
-          py::arg("options") = mm::GltfOptions{})
+          py::arg("options") = std::nullopt)
       .def(
           "add_mesh",
           &mm::GltfBuilder::addMesh,
