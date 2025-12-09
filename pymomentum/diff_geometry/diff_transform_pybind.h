62a232b8129b4f100f814c6f9f086299e6d30605
Chris Twigg <cdtwigg@meta.com>
2025-12-09 12:06:12 -0800

    Add new diff_geometry module. (#865)
    Summary:
    
    Adding a new diff_geometry module.
    
    The goal is to remove all torch/aten code from pymomentum.geometry and into a separate module.
    1. Create the new module.
    2. Port callers to the new module.
    3. Implement functionality in pymomentum.geometry using numpy instead of torch.
    
    This is working on step 1: we'll create a new diff_geometry module which wraps out all the same functions from pymomentum.geometry.  There are a few functions here with different names because we need them to be module-level functions in diff_geometry instead of class-level functions in pymomentum.geometry.
    
    Reviewed By: jeongseok-meta
    
    Differential Revision: D88600227

---
+++ pymomentum/diff_geometry/diff_transform_pybind.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <pybind11/pybind11.h>
+
+// Forward declaration for the functions defined in diff_transform_pybind.cpp
+void defParameterTransforms(pybind11::module_& m);
