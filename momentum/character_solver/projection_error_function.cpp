9831eb79b6053df2a58da1ce63472868f2dc1a6c
yutingye <1063619+yutingye@users.noreply.github.com>
2026-03-02 10:40:52 -0800

    Add JointErrorFunctionT, replacing ConstraintErrorFunctionT
    Summary:
    Renames ConstraintErrorFunctionT to JointErrorFunctionT and restructures it to
    inherit directly from SkeletonErrorFunctionT (removing the GeneralErrorFunctionT
    intermediate layer). Uses SkeletonDerivativeT for hierarchy walks in
    getGradient/getJacobian.
    
    Key changes:
    - ConstraintErrorFunctionT -> JointErrorFunctionT (new files: joint_error_function.h/inl.h)
    - Inherits SkeletonErrorFunctionT directly instead of GeneralErrorFunctionT
    - Leaf classes use new evalFunction(worldVecs) + getLocalVectors() interface
    - All 11 leaf classes updated: Position, Plane, Normal, Orientation, Aim*, FixedAxis*, Distance, Projection
    - Distance and Projection error functions now inherit JointErrorFunctionT (previously SkeletonErrorFunctionT)
    - GeneralizedLossT gets isL2()/invC2() accessors for optimization
    - diff_ik downstream updated for ProjectionErrorFunctionT member rename
    - External PointSplineErrorFunction (rocktenn/drita) updated
    
    Differential Revision: D94558392

--- momentum/character_solver/projection_error_function.cpp
+++ momentum/character_solver/projection_error_function.cpp
@@ -7,8 +7,7 @@
 
 #include "momentum/character_solver/projection_error_function.h"
 
-#include "momentum/character/skeleton.h"
-#include "momentum/character/skeleton_state.h"
+#include "momentum/math/utility.h"
 
 namespace momentum {
 
@@ -17,213 +16,57 @@ ProjectionErrorFunctionT<T>::ProjectionErrorFunctionT(
     const momentum::Skeleton& skel,
     const momentum::ParameterTransform& pt,
     T nearClip)
-    : SkeletonErrorFunctionT<T>(skel, pt), _nearClip(nearClip) {}
+    : Base(skel, pt), nearClip_(nearClip) {}
 
 template <typename T>
-double ProjectionErrorFunctionT<T>::getError(
-    const ModelParametersT<T>& /*parameters*/,
-    const SkeletonStateT<T>& skeletonState,
-    const MeshStateT<T>& /* meshState */) {
-  double error = 0;
+void ProjectionErrorFunctionT<T>::evalFunction(
+    size_t constrIndex,
+    const JointStateT<T>& state,
+    FuncType& f,
+    std::array<VType, 1>& v,
+    std::array<DfdvType, 1>& dfdv) const {
+  const auto& cons = this->constraints_[constrIndex];
 
-  const auto& jointState = skeletonState.jointState;
+  // Transform local offset to world space
+  v[0] = state.transform * cons.offset;
 
-  const auto nCons = constraints_.size();
-  for (size_t iCons = 0; iCons < nCons; ++iCons) {
-    const auto& cons = constraints_[iCons];
-    const auto& js = jointState[cons.parent];
-    const Eigen::Vector3<T> p_world_cm = js.transform * cons.offset;
-    const Eigen::Vector3<T> p_projected_cm =
-        constraints_[iCons].projection * p_world_cm.homogeneous();
+  // Project using the 3x4 projection matrix: (x, y, z) = P * [v; 1]
+  const Eigen::Vector3<T> p_projected = cons.projection * v[0].homogeneous();
 
-    // Behind camera:
-    if (p_projected_cm.z() < _nearClip) {
-      continue;
-    }
-
-    const Eigen::Vector2<T> diff = p_projected_cm.hnormalized().template head<2>() - cons.target;
-    error += cons.weight * kProjectionWeight * this->weight_ * diff.squaredNorm();
+  // Check near-clip plane - if behind camera, return zero residual to skip this constraint
+  if (p_projected.z() < nearClip_) {
+    f.setZero();
+    dfdv[0].setZero();
+    return;
   }
 
-  return error;
-}
+  const T z = p_projected.z();
 
-template <typename T>
-void ProjectionErrorFunctionT<T>::addConstraint(const ProjectionConstraintDataT<T>& data) {
-  constraints_.push_back(data);
-}
+  // Compute 2D residual: f = (x/z, y/z) - target
+  f = p_projected.template head<2>() / z - cons.target;
 
-template <typename T>
-double ProjectionErrorFunctionT<T>::getGradient(
-    const momentum::ModelParametersT<T>& /*params*/,
-    const momentum::SkeletonStateT<T>& skeletonState,
-    const momentum::MeshStateT<T>& /* meshState */,
-    Eigen::Ref<Eigen::VectorX<T>> gradient) {
-  double error = 0;
+  // The chain rule: df/dv = df/dp_projected * dp_projected/dv
+  // where p_projected = projection * [v; 1]
+  //
+  // Let P = projection (3x4), then p_projected = P * [v; 1] = P_33 * v + p_3
+  // where P_33 is the top-left 3x3 block and p_3 is the last column
+  //
+  // f = (x/z, y/z) - target, so:
+  // df/dp_projected = [(1/z, 0, -x/z^2), (0, 1/z, -y/z^2)]
+  //
+  // Then df/dv = df/dp_projected * P_33
 
-  for (size_t iCons = 0; iCons < constraints_.size(); ++iCons) {
-    const auto& cons = constraints_[iCons];
+  const T z_sqr = sqr(z);
+  const T x_zz = p_projected.x() / z_sqr;
+  const T y_zz = p_projected.y() / z_sqr;
+  const T inv_z = T(1) / z;
 
-    const auto& jsCons = skeletonState.jointState[cons.parent];
-    const Eigen::Vector3<T> p_world_cm = jsCons.transform * cons.offset;
-    const Eigen::Vector3<T> p_projected_cm = cons.projection * p_world_cm.homogeneous();
+  // df/dp_projected is 2x3
+  Eigen::Matrix<T, 2, 3> df_dp;
+  df_dp << inv_z, T(0), -x_zz, T(0), inv_z, -y_zz;
 
-    // Behind camera:
-    if (p_projected_cm.z() < _nearClip) {
-      continue;
-    }
-
-    const Eigen::Vector2<T> p_res = p_projected_cm.hnormalized() - cons.target;
-
-    error += cons.weight * kProjectionWeight * this->weight_ * p_res.squaredNorm();
-    const T wgt = 2.0f * cons.weight * kProjectionWeight * this->weight_;
-
-    const T z = p_projected_cm(2);
-    const T z_sqr = sqr(z);
-    const T x_zz = p_projected_cm(0) / z_sqr;
-    const T y_zz = p_projected_cm(1) / z_sqr;
-
-    auto addGradient = [&](const size_t jFullBodyDOF, const Eigen::Vector3<T>& d_p_world_cm) {
-      const Eigen::Vector3<T> d_p_projected =
-          cons.projection.template topLeftCorner<3, 3>() * d_p_world_cm;
-      const T dx = d_p_projected(0);
-      const T dy = d_p_projected(1);
-      const T dz = d_p_projected(2);
-
-      const Eigen::Vector2<T> d_p_res(dx / z - x_zz * dz, dy / z - y_zz * dz);
-      const T gradFull = d_p_res.dot(p_res);
-
-      // multiply by the parameter transform:
-      for (momentum::SparseRowMatrixf::InnerIterator it(
-               this->parameterTransform_.transform, (int)jFullBodyDOF);
-           it;
-           ++it) {
-        assert(static_cast<size_t>(it.row()) == jFullBodyDOF);
-        const auto jReducedDOF = it.col();
-        gradient(jReducedDOF) += it.value() * wgt * gradFull;
-      }
-    };
-
-    // loop over all joints the constraint is attached to and calculate gradient
-    size_t jointIndex = cons.parent;
-    while (jointIndex != kInvalidIndex) {
-      // check for valid index
-      assert(jointIndex < this->skeleton_.joints.size());
-
-      const auto& jointState = skeletonState.jointState[jointIndex];
-      const size_t paramIndex = jointIndex * 7;
-      const Eigen::Vector3<T> posd_cm = p_world_cm - jointState.translation();
-
-      // calculate derivatives based on active joints
-      for (size_t d = 0; d < 3; d++) {
-        if (this->activeJointParams_[paramIndex + d]) {
-          addGradient(paramIndex + d, jointState.getTranslationDerivative(d));
-        }
-        if (this->activeJointParams_[paramIndex + 3 + d]) {
-          addGradient(paramIndex + 3 + d, jointState.getRotationDerivative(d, posd_cm));
-        }
-      }
-      if (this->activeJointParams_[paramIndex + 6]) {
-        addGradient(paramIndex + 6, jointState.getScaleDerivative(posd_cm));
-      }
-
-      // go to the next joint
-      jointIndex = this->skeleton_.joints[jointIndex].parent;
-    }
-  }
-
-  return error;
-}
-
-template <typename T>
-double ProjectionErrorFunctionT<T>::getJacobian(
-    const ModelParametersT<T>& /*parameters*/,
-    const SkeletonStateT<T>& skeletonState,
-    const MeshStateT<T>& /* meshState */,
-    Eigen::Ref<Eigen::MatrixX<T>> jacobian,
-    Eigen::Ref<Eigen::VectorX<T>> residual,
-    int& usedRows) {
-  double error = 0;
-
-  for (size_t iCons = 0; iCons < constraints_.size(); ++iCons) {
-    const auto& cons = constraints_[iCons];
-
-    const auto& jsCons = skeletonState.jointState[cons.parent];
-    const Eigen::Vector3<T> p_world_cm = jsCons.transform * cons.offset;
-    const Eigen::Vector3<T> p_projected_cm = cons.projection * p_world_cm.homogeneous();
-
-    // Behind camera:
-    if (p_projected_cm.z() < _nearClip) {
-      continue;
-    }
-
-    const Eigen::Vector2<T> p_res = p_projected_cm.hnormalized() - cons.target;
-    error += cons.weight * kProjectionWeight * this->weight_ * p_res.squaredNorm();
-
-    const T z = p_projected_cm(2);
-    const T z_sqr = sqr(z);
-    const T x_zz = p_projected_cm(0) / z_sqr;
-    const T y_zz = p_projected_cm(1) / z_sqr;
-
-    const T wgt = std::sqrt(cons.weight * kProjectionWeight * this->weight_);
-    residual.template segment<2>(2 * iCons) = (wgt * p_res).template cast<T>();
-
-    auto addJacobian = [&](const size_t jFullBodyDOF, const Eigen::Vector3<T>& d_p_world_cm) {
-      const Eigen::Vector3<T> d_p_projected =
-          cons.projection.template topLeftCorner<3, 3>() * d_p_world_cm;
-      const T dx = d_p_projected(0);
-      const T dy = d_p_projected(1);
-      const T dz = d_p_projected(2);
-
-      const Eigen::Vector2<T> d_p_res(dx / z - x_zz * dz, dy / z - y_zz * dz);
-
-      // multiply by the parameter transform:
-      for (momentum::SparseRowMatrixf::InnerIterator it(
-               this->parameterTransform_.transform, (int)jFullBodyDOF);
-           it;
-           ++it) {
-        assert(static_cast<size_t>(it.row()) == jFullBodyDOF);
-        const auto jReducedDOF = it.col();
-        jacobian.template block<2, 1>(2 * iCons, jReducedDOF) += it.value() * wgt * d_p_res;
-      }
-    };
-
-    // loop over all joints the constraint is attached to and calculate gradient
-    size_t jointIndex = cons.parent;
-    while (jointIndex != kInvalidIndex) {
-      // check for valid index
-      assert(jointIndex < this->skeleton_.joints.size());
-
-      const auto& jointState = skeletonState.jointState[jointIndex];
-      const size_t paramIndex = jointIndex * 7;
-      const Eigen::Vector3<T> posd_cm = p_world_cm - jointState.translation();
-
-      // calculate derivatives based on active joints
-      for (size_t d = 0; d < 3; d++) {
-        if (this->activeJointParams_[paramIndex + d]) {
-          addJacobian(paramIndex + d, jointState.getTranslationDerivative(d));
-        }
-        if (this->activeJointParams_[paramIndex + 3 + d]) {
-          addJacobian(paramIndex + 3 + d, jointState.getRotationDerivative(d, posd_cm));
-        }
-      }
-      if (this->activeJointParams_[paramIndex + 6]) {
-        addJacobian(paramIndex + 6, jointState.getScaleDerivative(posd_cm));
-      }
-
-      // go to the next joint
-      jointIndex = this->skeleton_.joints[jointIndex].parent;
-    }
-  }
-
-  usedRows = static_cast<int>(jacobian.rows());
-
-  return error;
-}
-
-template <typename T>
-size_t ProjectionErrorFunctionT<T>::getJacobianSize() const {
-  return 2 * constraints_.size();
+  // Multiply by P_33 (top-left 3x3 of projection matrix)
+  dfdv[0] = df_dp * cons.projection.template topLeftCorner<3, 3>();
 }
 
 template class ProjectionErrorFunctionT<float>;
