ecac0962c507ff02b4b4413d006a1e8185eb7c33
yutingye <1063619+yutingye@users.noreply.github.com>
2026-03-22 00:17:17 -0700

    Audit comments in simd/ (#1185)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/momentum/pull/1185
    
    Audit and improve code comments in the simd/ module:
    * Fix outdated/inaccurate comments
    * Remove filler comments (constructor labels, restatements, etc.)
    * Add documentation for complex algorithms and non-obvious logic
    * Add actionable TODO notes where code needs improvement
    * Standardize Doxygen docstrings
    
    Differential Revision: D96091103

--- momentum/simd/simd.h
+++ momentum/simd/simd.h
@@ -18,30 +18,50 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
+/// Checks if DrJit version is greater than or equal to the specified version.
+/// @param major Major version number
+/// @param minor Minor version number
+/// @param patch Patch version number
+/// @return True if current DrJit version >= major.minor.patch
 #define DRJIT_VERSION_GE(major, minor, patch)                           \
   ((DRJIT_VERSION_MAJOR > (major)) ||                                   \
    (DRJIT_VERSION_MAJOR == (major) && DRJIT_VERSION_MINOR > (minor)) || \
    (DRJIT_VERSION_MAJOR == (major) && DRJIT_VERSION_MINOR == (minor) && \
     DRJIT_VERSION_PATCH >= (patch)))
 
-// Utilities for writing cross-platform SIMD code.
-// This currently uses the DrJit library for SIMD primitives.
+/// @file
+/// SIMD type aliases and mixed Eigen/DrJit arithmetic operators.
+///
+/// Provides fixed-width SIMD packet types (via DrJit) and overloaded
+/// operators for interoperating between Eigen vectors/matrices/transforms
+/// and DrJit SIMD packets.
 
 namespace momentum {
 
+/// Number of floats in an AVX-256 register.
 inline constexpr size_t kAvxPacketSize = 8;
+
+/// Byte alignment required for AVX-256 loads/stores.
 inline constexpr size_t kAvxAlignment = kAvxPacketSize * sizeof(float);
 
+/// Number of floats in the platform's default SIMD register (chosen by DrJit).
 inline constexpr size_t kSimdPacketSize = drjit::DefaultSize;
+
+/// Byte alignment required for the platform's default SIMD loads/stores.
 inline constexpr size_t kSimdAlignment = kSimdPacketSize * sizeof(float);
 
+/// A fixed-size SIMD packet of kSimdPacketSize elements of type T.
 template <typename T>
 using Packet = drjit::Packet<T, kSimdPacketSize>;
 
+/// @name Scalar packet aliases
+/// @{
 using FloatP = Packet<float>;
 using DoubleP = Packet<double>;
 using IntP = Packet<int>;
+/// @}
 
+/// A Dim-dimensional vector where each component is a SIMD packet.
 template <typename T, int Dim>
 using VectorP = drjit::Array<Packet<T>, Dim>;
 
@@ -51,13 +71,14 @@ using Vector2P = VectorP<T, 2>;
 template <typename T>
 using Vector3P = VectorP<T, 3>;
 
+/// @name Float and double 2D/3D vector packet aliases
+/// @{
 using Vector2fP = Vector2P<float>;
 using Vector3fP = Vector3P<float>;
 
 using Vector2dP = Vector2P<double>;
 using Vector3dP = Vector3P<double>;
-
-/// Additional packet types for byte and unsigned integer data.
+/// @}
 using ByteP = Packet<uint8_t>;
 using UintP = Packet<uint32_t>;
 
@@ -89,6 +110,9 @@ using Matrix4fP = Matrix4P<float>;
 using Matrix4dP = Matrix4P<double>;
 
 /// Computes the offset required for the matrix data to meet the alignment requirement.
+///
+/// @param mat The matrix whose alignment offset to compute.
+/// @return The number of elements to skip to achieve proper alignment.
 template <size_t Alignment>
 [[nodiscard]] size_t computeOffset(const Eigen::Ref<Eigen::MatrixXf>& mat) {
   constexpr size_t sizeOfScalar = sizeof(typename Eigen::MatrixXf::Scalar);
@@ -104,6 +128,9 @@ template <size_t Alignment>
 }
 
 /// Checks if the data of the matrix is aligned correctly.
+///
+/// @param mat The matrix to check for alignment.
+/// @param offset Offset into the matrix data in elements.
 template <size_t Alignment>
 void checkAlignment(const Eigen::Ref<Eigen::MatrixXf>& mat, size_t offset = 0) {
   MT_THROW_IF(
@@ -115,117 +142,131 @@ void checkAlignment(const Eigen::Ref<Eigen::MatrixXf>& mat, size_t offset = 0) {
       Alignment);
 }
 
-/// Calculates dot product of Eigen::Vector3f and 3-vector of packets.
+/// Computes dot product of Eigen::Vector3f and 3-vector of packets.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3D vector of Eigen.
-/// @param[in] v2 A 3-vector of packets.
+/// @param v1 A 3D vector of Eigen.
+/// @param v2 A 3-vector of packets.
+/// @return Dot product as a packet of floats.
 template <typename Derived, typename S>
 FloatP dot(const Eigen::MatrixBase<Derived>& v1, const Vector3P<S>& v2) {
   return drjit::fmadd(v1.x(), v2.x(), drjit::fmadd(v1.y(), v2.y(), v1.z() * v2.z()));
 }
 
-/// Calculates dot product of Vector3P and Eigen::Vector3f.
+/// Computes dot product of Vector3P and Eigen::Vector3f.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3-vector of packets.
-/// @param[in] v2 A 3D vector of Eigen.
+/// @param v1 A 3-vector of packets.
+/// @param v2 A 3D vector of Eigen.
+/// @return Dot product as a packet of floats.
 template <typename Derived, typename S>
 FloatP dot(const Vector3P<S>& v1, const Eigen::MatrixBase<Derived>& v2) {
   return drjit::fmadd(v1.x(), v2.x(), drjit::fmadd(v1.y(), v2.y(), v1.z() * v2.z()));
 }
 
-/// Calculates summation of Eigen::Vector3f and Vector3P
+/// Adds Eigen::Vector3f and Vector3P element-wise.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3D vector of Eigen.
-/// @param[in] v2 A 3-vector of packets.
+/// @param v1 A 3D vector of Eigen.
+/// @param v2 A 3-vector of packets.
+/// @return Element-wise sum as a 3-vector of packets.
 template <typename S, typename Derived>
 Vector3P<S> operator+(const Eigen::MatrixBase<Derived>& v1, const Vector3P<S>& v2) {
   return {v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z()};
 }
 
-/// Calculates summation of Vector3P and Eigen::Vector3f
+/// Adds Vector3P and Eigen::Vector3f element-wise.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3-vector of packets.
-/// @param[in] v2 A 3D vector of Eigen.
+/// @param v1 A 3-vector of packets.
+/// @param v2 A 3D vector of Eigen.
+/// @return Element-wise sum as a 3-vector of packets.
 template <typename S, typename Derived>
 Vector3P<S> operator+(const Vector3P<S>& v1, const Eigen::MatrixBase<Derived>& v2) {
   return {v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z()};
 }
 
-/// Calculates subtraction of Eigen::Vector3f and Vector3P
+/// Subtracts Vector3P from Eigen::Vector3f element-wise.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3D vector of Eigen.
-/// @param[in] v2 A 3-vector of packets.
+/// @param v1 A 3D vector of Eigen.
+/// @param v2 A 3-vector of packets.
+/// @return Element-wise difference as a 3-vector of packets.
 template <typename S, typename Derived>
 Vector3P<S> operator-(const Eigen::MatrixBase<Derived>& v1, const Vector3P<S>& v2) {
   return {v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z()};
 }
 
-/// Calculates subtraction of Vector3P and Eigen::Vector3f
+/// Subtracts Eigen::Vector3f from Vector3P element-wise.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3-vector of packets.
-/// @param[in] v2 A 3D vector of Eigen.
+/// @param v1 A 3-vector of packets.
+/// @param v2 A 3D vector of Eigen.
+/// @return Element-wise difference as a 3-vector of packets.
 template <typename S, typename Derived>
 Vector3P<S> operator-(const Vector3P<S>& v1, const Eigen::MatrixBase<Derived>& v2) {
   return {v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z()};
 }
 
-/// Calculates multiplication of quaternion and each 3x1 vector in packet
+/// Applies quaternion rotation to each 3x1 vector in packet.
 ///
-/// @param[in] q A quaternion.
-/// @param[in] vec A 3-vector of packets.
+/// Delegates to DrJit's quaternion-vector multiplication via ADL.
+///
+/// @param q A quaternion.
+/// @param vec A 3-vector of packets.
+/// @return Rotated vectors as a 3-vector of packets.
 template <typename S>
 Vector3P<S> operator*(const Eigen::Quaternion<S>& q, const Vector3P<S>& vec) {
+  // DrJit provides quaternion-packet multiplication via overload resolution
   return q * vec;
 }
 
-/// Calculates multiplication of 3x3 matrix and each 3x1 vector in packet
+/// Applies 3x3 matrix transformation to each 3x1 vector in packet.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Matrix3f.
-/// @param[in] xf A 3x3 matrix.
-/// @param[in] vec A 3-vector of packets.
+/// @param xf A 3x3 matrix.
+/// @param vec A 3-vector of packets.
+/// @return Transformed vectors as a 3-vector of packets.
 template <typename S, typename Derived>
 Vector3P<S> operator*(const Eigen::MatrixBase<Derived>& xf, const Vector3P<S>& vec) {
   return Vector3P<S>{
       momentum::dot(xf.row(0), vec), momentum::dot(xf.row(1), vec), momentum::dot(xf.row(2), vec)};
 }
 
-/// Calculates affine transformation on each 3x1 vector in packet
+/// Applies affine transformation to each 3x1 vector in packet.
 ///
-/// @param[in] xf An affine transformation matrix.
-/// @param[in] vec A 3-vector of packets.
+/// @param xf An affine transformation matrix.
+/// @param vec A 3-vector of packets.
+/// @return Transformed vectors as a 3-vector of packets.
 template <typename S>
 Vector3P<S> operator*(const Eigen::Transform<S, 3, Eigen::Affine>& xf, const Vector3P<S>& vec) {
   return momentum::operator+(momentum::operator*(xf.linear(), vec), xf.translation());
 }
 
-/// Calculates transformation on each 3x1 vector in packet using momentum::Transform
+/// Applies momentum::Transform to each 3x1 vector in packet.
 ///
-/// @param[in] xf A momentum transform.
-/// @param[in] vec A 3-vector of packets.
+/// @param xf A momentum transform.
+/// @param vec A 3-vector of packets.
+/// @return Transformed vectors as a 3-vector of packets.
 template <typename S>
 Vector3P<S> operator*(const TransformT<S>& xf, const Vector3P<S>& vec) {
   return momentum::operator+(momentum::operator*(xf.toLinear(), vec), xf.translation);
 }
 
-/// Calculates cross product of Eigen::Vector3f and Vector3P
+/// Computes cross product of Eigen::Vector3f and Vector3P.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3D vector of Eigen.
-/// @param[in] v2 A 3-vector of packets.
+/// @param v1 A 3D vector of Eigen.
+/// @param v2 A 3-vector of packets.
+/// @return Cross product as a 3-vector of packets.
 template <typename S, typename Derived>
 Vector3P<S> cross(const Eigen::MatrixBase<Derived>& v1, const Vector3P<S>& v2) {
   return {
@@ -235,12 +276,13 @@ Vector3P<S> cross(const Eigen::MatrixBase<Derived>& v1, const Vector3P<S>& v2) {
   };
 }
 
-/// Calculates cross product of Vector3P and Eigen::Vector3f
+/// Computes cross product of Vector3P and Eigen::Vector3f.
 ///
 /// @tparam Derived A derived class of Eigen::MatrixBase, which should be compatible with
 /// Eigen::Vector3f.
-/// @param[in] v1 A 3-vector of packets.
-/// @param[in] v2 A 3D vector of Eigen.
+/// @param v1 A 3-vector of packets.
+/// @param v2 A 3D vector of Eigen.
+/// @return Cross product as a 3-vector of packets.
 template <typename S, typename Derived>
 Vector3P<S> cross(const Vector3P<S>& v1, const Eigen::MatrixBase<Derived>& v2) {
   return {
